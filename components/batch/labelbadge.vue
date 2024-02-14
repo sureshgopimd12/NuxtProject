fa259b2e65503dd50d2979f828c7578646a8f5d2
kiruba <kirubaselviten@outlook.com>
2024-02-14 10:32:07 +0530

    updated components

---
+++ components/batch/labelbadge.vue
@@ -0,0 +1,100 @@
+<template>
+
+<h1> Label Badges</h1>
+    <div class="primary">Primary</div>
+    <div class="secondary">Secondary</div>
+    <div class="Success">Success</div>
+    <div class="Danger">Danger</div>
+    <div class="Warning">Warning</div>
+    <div class="Info">Info</div>
+    <div class="Dark"> Dark</div>
+    
+    
+    
+    
+    </template>
+    
+    
+    
+    <style scoped>
+    
+    .primary{
+        
+        background-color:#eae8fd  ;
+        display:inline-block;
+        width: auto;
+        color:#7367f0 ;
+        padding:5px; 
+        border-radius:4px;
+    }
+    .secondary{
+    
+        background-color:#f2f2f3 ;
+        display:inline-block;
+        width: auto;
+        color: #a8aaae;
+        padding:5px; 
+        border-radius:4px;
+    
+    }
+    
+    .Success{
+    
+        
+        background-color:#dff7e9  ;
+        display:inline-block;
+        width: auto;
+        color: #28c76f;
+        padding:5px; 
+        border-radius:4px;
+    }
+    
+    
+    .Danger{
+    
+        
+        background-color:#f14e4e ;
+        display:inline-block;
+        width: auto;
+        color: white;
+        padding:5px; 
+        border-radius:4px;
+    }
+    
+    
+    .Warning{
+    
+        
+    background-color:#fff1e3  ;
+    display:inline-block;
+    width: auto;
+    color: #ea5455;
+    padding:5px; 
+    border-radius:4px;
+    }
+    
+    
+    .Info{
+    
+        
+    background-color:#d9f8fc ;
+    display:inline-block;
+    width: auto;
+    color: #00cfe8;
+    padding:5px; 
+    border-radius:4px;
+    }
+    
+    
+    .Dark{
+    
+        
+    background-color:#e4e4e4;
+    display:inline-block;
+    width: auto;
+    color: #4b4b4b ;
+    padding:5px; 
+    border-radius:4px;
+    }
+    
+    </style>
---
+++ components/collapsables/collapsables.vue
@@ -0,0 +1,57 @@
+<template>
+<div >
+
+<button class="colapse-button" @click="hide=!hide"  >collapse the content</button>
+
+
+
+<Transition   :duration="550">
+  <div v-if="hide"  class=" content ">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras felis felis, malesuada in nulla id, sollicitudin dictum augue. Vestibulum condimentum, dui at consectetur consequat, magna turpis rhoncus sem, ut eleifend massa quam et nunc. Vivamus non accumsan nisi, vel dignissim diam.
+</div>
+</Transition>
+
+</div>
+
+
+</template>
+
+
+<style scoped>
+
+
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+
+.colapse-button{
+padding: 2px;
+border-radius: 5px;
+border: solid;
+border-color: #7f6ebd;
+box-shadow:0 0.125rem 0.25rem rgba(165,163,174,.3);
+color: aliceblue;
+background-color: #7f6ebd;
+}
+
+
+
+.content{
+  border: black;
+  padding: 8px;
+  border-style: solid;
+  margin: 6px;
+}
+</style>
+
+<script setup lang="ts">
+const hide =ref(true)
+</script>
