a6b4f26d02b1419367a86503d9ab0a79668f473c
kiruba <kirubaselviten@outlook.com>
2024-02-14 10:30:03 +0530

    updated components

--- app.vue
+++ app.vue
@@ -1,5 +1,72 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+
+
+
+
+<link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@100&display=swap" rel="stylesheet">
+
+
+
+<h1> Alert Boxes </h1>
+
+<alert-basicalert/>
+<alert-linkalert/>
+<alert-dismissablealert/>
+<batch/>
+<batch-labelbadge/>
+<batch-notifications/>
+<collapsables/>
+<linkalert/>S
+
+  
+
+
 </template>
+<style >
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Public Sans";
+  
+}
+
+
+h1,h1,h3,h4,h5,h6{
+  font-family:'Public Sans' ;
+  color:#5d596c;
+  font-weight: 500;
+  line-height: 1.37;
+}
+
+h5{
+  font-size: 1.125rem;
+}
+body{
+  background-color: #f8f7fa;
+}
+
+
+small{
+  color: #a5a3ae;
+ 
+  opacity: 1;
+}
+
+a{
+
+  text-decoration: none;
+  font-weight: 700;
+}
+
+</style>
+
+
+<script setup lang="ts">
+
+
+
+
+
+
+</script>
